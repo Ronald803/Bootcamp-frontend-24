0141c8063aeff7607ae3d68d3aa2f58b917aab3c
Ronald803 <ronaldblancobalboa@gmail.com>
2024-03-12 17:55:28 -0400

    cinema oop js challenge

---
+++ JS-challenge-1.js
@@ -0,0 +1,36 @@
+class Seat {
+    constructor(rowPosition,seatPosition){
+        this.rowPosition = rowPosition;
+        this.seatPosition = seatPosition;
+        this.availability = true;
+    }
+}
+class Cinema {
+    constructor(rowsQuantity,seatsQuantity){
+        this.rowsQuantity = rowsQuantity;
+        this.seatsQuantity = seatsQuantity;
+        this.allSeats = [];
+        for (let i = 0; i < rowsQuantity; i++) {
+            for (let j = 0; j < seatsQuantity; j++) {
+                this.allSeats.push(new Seat(i,j))                
+            }
+        }
+    }
+    showSeatStatus(){
+        return this.allSeats
+    }
+    reservaSeat(row,seat){
+        let index = this.allSeats.findIndex(s=>(s.rowPosition==row && s.seatPosition==seat))
+        if(index != -1 ){
+            if(this.allSeats[index].availability === true){
+                this.allSeats[index].availability = false
+                console.log("Seat reserved"); 
+            } else {
+                console.log("Seat not available");
+            }
+            
+        }
+
+    }
+
+}
