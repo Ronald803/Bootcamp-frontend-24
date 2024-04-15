b11ebc0b0f5b55080202e4872f8a5db1d47c0f85
Ronald803 <ronaldblancobalboa@gmail.com>
2024-04-15 11:57:02 -0400

    Challenge Refactored: 1. Cinema seats logic changed, from an unidemensional array to a bidimensional array, in order to improve the performance of the app. 2. Names of the attributes of the class Seat changed in order to improve the legibility. 3. Tab size changed to 2, as a recommendation of the trainer in a past class of Frontend

--- JS-challenge-1.js
+++ JS-challenge-1.js
@@ -1,35 +1,39 @@
 class Seat {
-    constructor(rowPosition,seatPosition){
-        this.rowPosition = rowPosition;
-        this.seatPosition = seatPosition;
-        this.availability = true;
-    }
+  constructor(row,column){
+    this.row = row;
+    this.column = column;
+    this.availability = true;
+  }
 }
 class Cinema {
-    constructor(rowsQuantity,seatsQuantity){
-        this.rowsQuantity = rowsQuantity;
-        this.seatsQuantity = seatsQuantity;
-        this.allSeats = [];
-        for (let i = 0; i < rowsQuantity; i++) {
-            for (let j = 0; j < seatsQuantity; j++) {
-                this.allSeats.push(new Seat(i,j))                
-            }
-        }
+  constructor(rowsQuantity,seatsQuantity){
+    this.rowsQuantity = rowsQuantity;
+    this.seatsQuantity = seatsQuantity;
+    this.allSeats = [];
+    for (let i = 0; i < rowsQuantity; i++) {
+      let row = [];
+      for (let j = 0; j < seatsQuantity; j++) {
+          row.push(new Seat(i,j))                
+      }
+      this.allSeats.push(row)
     }
-    showSeatStatus(){
-        console.log(this.allSeats);
+  }
+  showSeatStatus(){
+    console.log(this.allSeats);
+  }
+  reservaSeat(row,column){
+    let seat = this.allSeats[row][column];
+    if(seat != undefined){
+      if(this.allSeats[row][column].availability === true){
+        this.allSeats[row][column].availability = false;
+        console.log("Seat reserved");
+      } else {
+        console.log("Seat not available");
+      }
+    } else {
+      console.log("The seat does not exist");
     }
-    reservaSeat(row,seat){
-        let index = this.allSeats.findIndex(s=>(s.rowPosition==row && s.seatPosition==seat))
-        if(index != -1 ){
-            if(this.allSeats[index].availability === true){
-                this.allSeats[index].availability = false
-                console.log("Seat reserved"); 
-            } else {
-                console.log("Seat not available");
-            } 
-        }
-    }
+  }
 }
 
 const cinema = new Cinema(5,10);
