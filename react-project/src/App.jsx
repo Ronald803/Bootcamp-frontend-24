3ff0ffae4229383ebfe6d59dd796de4ec0f69f62
Ronald803 <ronaldblancobalboa@gmail.com>
2024-04-24 18:05:08 -0400

    button added

---
+++ react-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Mybutton from './Mybutton'
+
+function App() {
+  return (
+    <>
+      <h1>Hello, I am Ronald</h1>
+      <Mybutton></Mybutton>
+    </>
+  )
+}
+
+export default App
---
+++ react-project/src/Mybutton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../src/myStyles.css'
+
+function Mybutton() {
+  return (
+    <button className='avatar'>I'm a button</button>
+  )
+}
+
+export default Mybutton
