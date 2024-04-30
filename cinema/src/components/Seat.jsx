9f9915956bc9266f4bd07d4fa236b8d69005e265
Ronald803 <ronaldblancobalboa@gmail.com>
2024-04-29 21:04:20 -0400

    challenge completed

--- cinema/src/components/Seat.jsx
+++ cinema/src/components/Seat.jsx
@@ -1,26 +1,20 @@
 import React, { useState } from 'react'
 
 function Seat(props) {
-    // let imgSrc = ''
-    // if(props.seat.availability){
-    //     imgSrc = '../src/assets/seat.png'
-    // } else {
-    //     imgSrc = '../src/assets/seatReserved.png'
-    // }
-    const [imgSrc, setImgSrc] = useState('../src/assets/seat.png')
-    const seatClick = ()=>{
-        props.cinema.reservaSeat(props.rowNumber,props.colNumber)
-        if(!props.seat.availability){
-            setImgSrc('../src/assets/seatReserved.png')
-        }
+  const [imgSrc, setImgSrc] = useState('../src/assets/seat.png')
+  const seatClick = ()=>{
+    props.cinema.reservaSeat(props.rowNumber,props.colNumber)
+    if(!props.seat.availability){
+      setImgSrc('../src/assets/seatReserved.png')
     }
+  }
   return (
     <div className='seat'>
-        <button>
-            <img 
-                src={imgSrc} 
-                onClick={()=>seatClick()}/>
-        </button>
+      <button>
+        <img 
+          src={imgSrc} 
+          onClick={()=>seatClick()}/>
+      </button>
     </div>
   )
 }
