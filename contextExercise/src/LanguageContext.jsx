a6b0ea18486963d69344407e9a86ab247a984a3d
Ronald803 <ronaldblancobalboa@gmail.com>
2024-05-13 18:42:17 -0400

    challenge complete

--- contextExercise/src/LanguageContext.jsx
+++ contextExercise/src/LanguageContext.jsx
@@ -1,11 +1,17 @@
 import React, { createContext, useState } from "react";
+import { languages } from "./LanguagesDB";
 
 const LanguagesContext = createContext();
 const LanguageContextProvider = ({children}) => {
-    const languages = ['JavaScript', 'Python'];
     const [language, setLanguage] = useState(languages[0])
     const toggleLanguage = () => {
-      setLanguage(languages[1])
+      let index = languages.findIndex(lang=>lang==language)
+      if(index == (languages.length - 1)){
+        index = 0
+      } else {
+        index = index + 1
+      }
+      setLanguage(languages[index])
     }
   return (
     <LanguagesContext.Provider value={{language,toggleLanguage}}>
